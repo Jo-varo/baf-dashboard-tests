ced5bca88818f168ec90612f0f75a1862bdced79
jorge020499 <jorge020499@gmail.com>
2025-06-12 23:31:25 +0000

    Re add import, fix styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Layout } from './Layout.tsx'
+// @ts-ignore
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
